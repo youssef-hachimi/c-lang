fc5d9debab4880923bec585eb3950cc373c5a203
youssef <83027476+youissef@users.noreply.github.com>
2024-01-04 22:55:31 +0100

    birthdayFunction.c

---
+++ 1/4-four/app1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// prototype:
+void birthday();
+int main()
+{
+   birthday();
+   birthday();
+   birthday();
+}
+   return 0;
+void birthday()
+{
+   printf("\nHappy birthday to you!");
+   printf("\nHappy birthday to you!");
+   printf("\nHappy birthday dear...YOU!");
+   printf("\nHappy birthday to you!\n");
+}
