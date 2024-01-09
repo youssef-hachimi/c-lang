95d2495639a4b346428acf7ea68c436cb4c50fa5
youssef <83027476+youissef@users.noreply.github.com>
2024-01-09 22:25:10 +0100

    Update app1.c

---
+++ 1/3-three/app1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdlib.h> 
+#define max 100;
+// global variables
+static int i;
+
+// structure def (new type)
+typedef struct
+{
+    int ref;
+    char mark[14];
+    int RAM;
+    int disk;
+    char os;
+} computer;
+
+
+// main function
+int main()
+{
+
+}
